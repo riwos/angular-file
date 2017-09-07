2000377ea9f3c3053413609c2b1a6d11ffe798b3
ackerapple <acker.dawn.apple@gmail.com>
2017-09-07 16:54:29 -0400

    0.0.0

--- dist/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# angular-file - Change Log
-All notable changes to this project will be documented here.
-
-## [1.3.0] - 2017-08-31
-### Overhauled by Acker
-- Taken from an outdated barely usable package to an up-to-date slimmed down easier to develop version
-## Added
-- uploader.getFormData()
-
--- src/file-upload/ngfSelect.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ElementRef, Output, Input, HostListener, EventEmitter } from '@angular/core';
-import { ngf } from "./ngf.directive"
-
-@Directive({selector: '[ngfSelect]'})
-export class ngfSelect extends ngf {
-  @Input() selectable = true
-  @Input('ngfSelect') ref:ngfSelect
-  @Output('ngfSelectChange') refChange:EventEmitter<ngfSelect> = new EventEmitter()  
-}
