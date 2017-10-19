b7c3b48f61d00fc380a58851c3fb6e31706e2193
ackerapple <acker.dawn.apple@gmail.com>
2017-10-19 12:34:51 -0400

    rebuilt

--- demo/src/app/components/file-upload/simple-demo.component.template.ts
+++ demo/src/app/components/file-upload/simple-demo.component.template.ts
@@ -1,9 +1,7 @@
-export const string = "import { Component } from '@angular/core';"+
-"\nimport { FileUploader } from '../../../../../src';"+
+export const string = "import { Component } from '@angular/core'"+
+"\nimport { Observable, Subscription } from 'rxjs'"+
 "\nimport { string as template } from \"./simple-demo.template\""+
-"\n"+
-"\n// const URL = '/api/';"+
-"\nconst URL = 'https://evening-anchorage-3159.herokuapp.com/api/';"+
+"\nimport { HttpClient, HttpRequest, HttpResponse, HttpEvent } from '@angular/common/http'"+
 "\n"+
 "\n@Component({"+
 "\n  selector: 'simple-demo',"+
@@ -11,16 +9,42 @@ export const string = "import { Component } from '@angular/core';"+
 "\n  template:template"+
 "\n})"+
 "\nexport class SimpleDemoComponent {"+
-"\n  public uploader:FileUploader = new FileUploader({url: URL});"+
-"\n  public hasBaseDropZoneOver:boolean = false;"+
-"\n  public hasAnotherDropZoneOver:boolean = false;"+
+"\n  files:File[] = []"+
+"\n  progress:number"+
+"\n  url = 'https://evening-anchorage-3159.herokuapp.com/api/'"+
+"\n  hasBaseDropZoneOver:boolean = false"+
+"\n  "+
+"\n  httpEmitter:Subscription"+
+"\n  httpEvent:HttpEvent<any>"+
 "\n"+
-"\n  public fileOverBase(e:any):void {"+
-"\n    this.hasBaseDropZoneOver = e;"+
+"\n  constructor(public HttpClient:HttpClient){}"+
+"\n"+
+"\n  cancel(){"+
+"\n    this.progress = 0"+
+"\n    if( this.httpEmitter ){"+
+"\n      console.log('cancelled')"+
+"\n      this.httpEmitter.unsubscribe()"+
+"\n    }"+
 "\n  }"+
 "\n"+
-"\n  public fileOverAnother(e:any):void {"+
-"\n    this.hasAnotherDropZoneOver = e;"+
+"\n  uploadFiles(files:File[]):Subscription{"+
+"\n    const formData:FormData = new FormData();"+
+"\n    for (let file of files) {"+
+"\n      formData.append('file', file, file.name)//input-name, file-contents, filename"+
+"\n    }"+
+"\n"+
+"\n    const req = new HttpRequest<FormData>('POST', this.url, formData, {"+
+"\n      reportProgress: true//, responseType: 'text'"+
+"\n    })"+
+"\n    "+
+"\n    return this.httpEmitter = this.HttpClient.request(req).subscribe(event=>{"+
+"\n      this.httpEvent = event"+
+"\n      "+
+"\n      if (event instanceof HttpResponse) {"+
+"\n        delete this.httpEmitter"+
+"\n        console.log('request done', event)"+
+"\n      }"+
+"\n    })"+
 "\n  }"+
 "\n}"+
 "\n"
