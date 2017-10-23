b2872822e3aff7f89fd8cc03ead1996c883f373e
ackerapple <acker.dawn.apple@gmail.com>
2017-10-23 13:07:24 -0400

    fix ie11

--- file-upload/ngfDrop.directive.js
+++ file-upload/ngfDrop.directive.js
@@ -37,9 +37,11 @@ var ngfDrop = /** @class */ (function (_super) {
         _super.prototype.handleFiles.call(this, files);
     };
     ngfDrop.prototype.onDragOver = function (event) {
+        var transfer = this.eventToTransfer(event);
+        var hasFiles = this.transferHasFiles(transfer);
         var files = this.eventToFiles(event);
-        if (!files.length)
-            return;
+        //IE11 does NOT tell you about dragged files. Always 0 files
+        //if(!files.length)return
         this.validDrag = this.uploader.isFilesValid(files);
         this.validDragChange.emit(this.validDrag);
         this.invalidDrag = !this.validDrag;
